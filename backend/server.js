374cd4712feac295ea652a424668d4dfc174fd2d
olfia <olfiarachmi@gmail.com>
2024-12-07 23:08:25 +0800

    1 - auth

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json()); //for parsing req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+});
